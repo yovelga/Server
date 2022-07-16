f8d60891f92f234c31da7ba855d5841bde576054
yovelga <85686263+yovelga@users.noreply.github.com>
2022-07-16 09:46:54 +0300

    Delete flaskProject1/pages/home directory

--- flaskProject1/pages/home/static/js/home.js
+++
@@ -1,8 +0,0 @@
-//calculate and print the average time the user listen to music according to it's input
-function calcTime() {
-  const numOfSongs = document.getElementById("inputNum").value;
-  var res = "you listen to songs for an average time of ";
-  res += numOfSongs*3.5;
-  res += " minutes a day";
-  document.getElementById("result").innerHTML = res;
-}
